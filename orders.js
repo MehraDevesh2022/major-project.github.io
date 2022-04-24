398183bc0ca5b4469c57196b1ac32be0dd91ee2d
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2022-04-24 23:35:31 +0530

    add js

---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders =[
+ {
+     productName : "Foldable mini Drone",
+     productNumber : "85631",
+     paymentStatus : "Due",
+     shipping: "Panding"
+ },   
+ {
+     productName : "LARVENDR KF102 Drone",
+     productNumber : "36378",
+     paymentStatus : "Redunded",
+     shipping: "Declined"
+ },   
+ {
+     productName : "Ruko F11 Pro Drone",
+     productNumber : "49347",
+     paymentStatus : "Due",
+     shipping: "Panding"
+ },   
+ {
+     productName : "Foldable mini Drone",
+     productNumber : "96996",
+     paymentStatus : "Paid",
+     shipping: "Deliverd"
+ },   
+ {
+     productName : "Foldable mini Drone",
+     productNumber : "22821",
+     paymentStatus : "Paid",
+     shipping: "Delivered"
+ },   
+ 
+]
